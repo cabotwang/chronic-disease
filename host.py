88f524619fc6e5c7473a54760730869824223f1d
cabotwang <sichen_wang@126.com>
2022-05-28 20:20:14 +0800

    Update host.py

--- host.py
+++ host.py
@@ -5,15 +5,14 @@ from data_input import datainputApp
 from model_explain import modelexplainApp
 
 st.set_page_config(layout="wide")
-
+for line in open('style.css', encoding='utf-8'):
+    st.markdown(f'<style>{line}</style>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
-    # this is the host application, we add children to it and that's it!
     st.title('来宾慢病管理平台')
     app = HydraApp(title='慢病管理平台', favicon="🏠", navbar_theme={'menu_background':'royalblue'})
     app.add_app("信息采集", icon="⌨", app=datainputApp())
     app.add_app("信息查询", icon="📚", app=datasearchApp())
     app.add_app("诊疗路径", icon="💬", app=modelexplainApp())
 
-    # run the whole lot
     app.run()
