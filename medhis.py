075a0029bd173428dd0abf247b2e80213c0b420a
cabotwang <sichen_wang@126.com>
2022-06-16 13:15:37 +0800

    Update medhis.py

--- medhis.py
+++ medhis.py
@@ -26,12 +26,17 @@ class medhisApp(HydraHeadApp):
             st.session_state.search_button_clicked = False
         if "add_info_button_clicked" not in st.session_state:
             st.session_state.add_info_button_clicked = False
+        if "second_opinion_button_clicked" not in st.session_state:
+            st.session_state.second_opinion_button_clicked = False
 
         def call_back_add_info():
             st.session_state.add_info_button_clicked = True
 
         def call_back_search():
             st.session_state.search_button_clicked = True
+
+        def call_back_second_opinion():
+            st.session_state.second_opinion_button_clicked = True
 
         ce, c1, ce, c2, c3, ce, c4, ce = st.columns([0.07, 0.8, 0.07, 0.8, 0.8, 0.07, 1.6, 0.07])
         person_name = c1.text_input('按姓名搜索')
@@ -71,6 +76,7 @@ class medhisApp(HydraHeadApp):
                     fee = c3.button('预览费用清单', key='fee_view')
                     if discharge:
                         st.session_state.add_info_button_clicked = False
+                        st.session_state.second_opinion_button_clicked = False
                         c4.markdown('<p class="label-font">出院小结</p>', unsafe_allow_html=True)
                         photos = deta.Drive(f'{ID}_discharge'.format(ID=person_id))
                         for value in photos.list().values():
@@ -79,6 +85,7 @@ class medhisApp(HydraHeadApp):
                             c4.image(image)
                     if fee:
                         st.session_state.add_info_button_clicked = False
+                        st.session_state.second_opinion_button_clicked = False
                         c4.markdown('<p class="label-font">费用清单</p>', unsafe_allow_html=True)
                         photos = deta.Drive(f'{ID}_fee'.format(ID=person_id))
                         for value in photos.list().values():
@@ -104,6 +111,7 @@ class medhisApp(HydraHeadApp):
                     pass
 
                 if c2.button('新增住院信息', on_click=call_back_add_info) or st.session_state.add_info_button_clicked:
+                    st.session_state.second_opinion_button_clicked = False
                     c4.markdown('<p class="label-font">住院信息</p>', unsafe_allow_html=True)
                     his_type = c4.selectbox('时间结点', ('入院后手术前', '手术后出院前', '出院当日/后⼀日'), key='his_type')
                     uploaded_discharge = None
@@ -132,7 +140,8 @@ class medhisApp(HydraHeadApp):
                             c4.text_input('请具体说明', key='surgery_type')
                         surgery_time = c4.date_input('手术时间', datetime.date(2022, 5, 26), key='surgery_time')
                         discharge_time = c4.date_input('预计出院时间', datetime.date(2022, 5, 26), key='discharge_time')
-                        extention_reson = c4.selectbox('是否有以下特征', ('无以下症状', '发热', '血尿', '头痛,恶心,呕吐', '年龄65岁以上', '腰部疼痛无缓解或者加剧'))
+                        extention_reson = c4.selectbox('是否有以下特征',
+                                                       ('无以下症状', '发热', '血尿', '头痛,恶心,呕吐', '年龄65岁以上', '腰部疼痛无缓解或者加剧'))
                     else:
                         activity_state = c4.selectbox('活动状态', ('自主活动', '活动受限', '被动活动：轴线翻⾝', '被动活动：下肢抬⾼'),
                                                       key='his_type')
@@ -183,10 +192,31 @@ class medhisApp(HydraHeadApp):
                         st.session_state.add_info_button_clicked = False
                         st.experimental_rerun()
 
-                if c2.button('增加第二诊疗意见'):
+                if c2.button('增加第二诊疗意见',
+                             on_click=call_back_second_opinion) or st.session_state.second_opinion_button_clicked:
+                    st.session_state.add_info_button_clicked = False
                     c4.markdown('<p class="label-font">第二诊疗意见</p>', unsafe_allow_html=True)
-                    therapy_2o = c4.selectbox('第二诊疗意见', ('保守治疗', '微创手术', '开发手术'), key='therapy_2o')
-                    c4.text_input()
+                    therapy_2o = c4.selectbox('第二诊疗意见', ('保守治疗', '微创手术', '开放手术'), key='therapy_2o')
+                    if therapy_2o == '微创手术':
+                        therapy_name = c4.selectbox('术式', ('腰椎融合术', '腰椎人工椎间盘置换术', '其他'), key='surgery_name')
+                    elif therapy_2o == '开放手术':
+                        therapy_name = c4.selectbox('术式', ('经皮椎间盘切吸术', '经皮椎间盘激光消融术', '经皮椎间盘臭氧消融术及射频消融髓核成形术',
+                                                           '经皮椎间盘等离子消融术', '经皮椎间盘胶原酶化学溶解术', '显微腰椎间盘切除术',
+                                                           '显微内窥镜腰椎间盘切除术', '经皮内镜腰椎间盘切除术', '椎间盘髓核摘除术', '其他'),
+                                                    key='surgery_name')
+                    elif therapy_2o == '保守治疗':
+                        therapy_name = c4.selectbox('诊疗方式', ('药物', '运动疗法', '康复训练', '牵引治疗', '矫形器装配体外冲击波治疗', '电刺激疗法',
+                                                             '激光治疗', '针灸治疗', '推拿按摩', '拔罐治疗', '刮痧治疗', '其他'),
+                                                    key='surgery_name')
+                    expert_name = c4.text_input('专家姓名')
+                    report_time = c4.date_input('报告时间', datetime.date(2022, 5, 26), key='therapy_time')
+                    if c4.button('确认'):
+                        db_medhis = deta.Base("medhis_info")
+                        db_medhis.update({'key': ID, '第二诊疗意见': {therapy_2o: {'诊疗名称': therapy_name, '专家姓名': expert_name,
+                                                                             '报告时间': report_time}},
+                                          '数据采集时间': datetime.datetime.now().strftime("%Y-%m-%d")})
+                        st.session_state.second_opinion_button_clicked = False
+
 
             except IndexError:
                 st.error('不存在患者信息，请进行院前信息填报流程')
